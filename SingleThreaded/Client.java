b96f06382e790987d9c6123f521836a14796feaf
sehajmakkar <codewithsehaj@gmail.com>
2025-07-01 01:07:47 +0530

    single threaded web server complete!

--- SingleThreaded/Client.java
+++ SingleThreaded/Client.java
@@ -1,3 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
-  
+  public void run() throws UnknownHostException, IOException{
+    // client socket
+    int port = 8010;
+    InetAddress address = InetAddress.getByName("localhost");
+    Socket socket = new Socket(address, port);
+
+    // client to server
+    PrintWriter toServer = new PrintWriter(socket.getOutputStream());
+
+    // server to client.
+    BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+    String serverMessage = fromServer.readLine();
+    System.out.println("Response from server: " + serverMessage);
+
+    // close the streams
+    toServer.close();
+    fromServer.close();
+    socket.close();
+    System.out.println("Connection closed with " + socket.getRemoteSocketAddress() + ":" + socket.getPort());
+    
+  }
+
+  public static void main(String[] args) {
+    try{
+      Client client = new Client();
+      client.run();
+
+    }catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
--- SingleThreaded/Server.java
+++ SingleThreaded/Server.java
@@ -1,3 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+  public void run() throws IOException {
+      // server socket
+      int port = 8010;
+      ServerSocket socket = new ServerSocket(port);
+      socket.setSoTimeout(10000); // 10sec
+
+      while(true){
+        try {
+          System.out.println("Server is listening on " + port);
+
+          // ready and waiting to accept a connection
+          Socket acceptedConnection = socket.accept();
+
+          System.out.println("Connection accepted from " + acceptedConnection.getRemoteSocketAddress() + ":" + acceptedConnection.getPort());
+
+          // server to client
+          PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream()); // text to bytes
+
+          // client to server
+          BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream())); // bytes to text
+
+          // sent to the client
+          toClient.println("Hello from the server!");
+
+          // closing the streams
+          toClient.close();
+          fromClient.close();
+          acceptedConnection.close();
+          socket.close();
+          System.out.println("Connection closed with " + acceptedConnection.getRemoteSocketAddress() + ":" + acceptedConnection.getPort());
+
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    
+  }
   
+  public static void main(String[] args) {
+    Server server = new Server();
+    try{
+      server.run();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
